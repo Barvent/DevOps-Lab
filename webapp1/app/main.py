c297dcd65d531795c5cf8e9d09bc3bc34ce5cb25
Guillem Reig <67266826+g-reig@users.noreply.github.com>
2023-10-27 17:26:51 +0200

    Test sonarcloud integration

--- webapp1/app/main.py
+++ webapp1/app/main.py
@@ -1,8 +1,10 @@
 from flask import Flask
 import socket
+import os
 
 app = Flask(__name__)
 
 @app.route("/bye")
 def bye_world():
+    os.system('ls')
     return "Bye! I am {} by the way.".format(socket.gethostname())
