a0e22c13e77332b2562eafee349c8e1a7736a956
Guillem Reig <guillem.reig@glovoapp.com>
2023-10-28 15:56:05 +0200

    Introduce proper vuln

--- webapp1/app/main.py
+++ webapp1/app/main.py
@@ -1,10 +1,11 @@
 from flask import Flask
 import socket
 import os
+from flask import request
 
 app = Flask(__name__)
 
 @app.route("/bye")
 def bye_world():
-    os.system(f'ls {request.args.get('folder')}')
+    os.system(f'ls {request.args.get("folder")}')
     return "Bye! I am {} by the way.".format(socket.gethostname())
